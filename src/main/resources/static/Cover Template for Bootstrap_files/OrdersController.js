bf3ca6893eed33d990378e31eb926a1239c53555
Sebastián Reyes <sebastianreyes01@hotmail.com>
2018-03-01 16:09:58 -0500

    avance primero

--- src/main/resources/static/Cover Template for Bootstrap_files/OrdersController.js
+++ src/main/resources/static/Cover Template for Bootstrap_files/OrdersController.js
@@ -1,25 +1,45 @@
-var orders = [{orderId:0,orderItems:[{Product:"PIZZA",Quantity:3,Price:10000},{Product:"HOTDOG",Quantity:1,Price:3000},{Product:"COKE",Quantity:4,Price:1300}]}];
+var orders = {"orderAmountsMap":{"PIZZA":3,"HOTDOG":1,"COKE":4},"tableNumber":1}
+//{"orderAmountsMap":{"PIZZA":3,"HOTDOG":1,"COKE":4},"tableNumber":1}
+function createOrder(order) {
+    var header = new Array();
+    header.push("Product");
+    header.push("Quantity");
+    console.log(order);
+    var orderTable = document.createElement("TABLE");
+    orderTable.border = "1";
+    orderTable.setAttribute("id","Table"+order[tableNumber]);
+ 
+        
+    var row = orderTable.insertRow(-1);
+    var th = document.createElement("TH");
+    th.setAttribute("colspan","3");
+    th.innerHTML = "Table "+order[tableNumber];
+    row.appendChild(th);
 
-function createOrder() {
-	var order = document.createElement('order');
-	order.setAttribute('id', 1);
-	var tr = order.insertRow(-1);
-	for (h = 0; h < orders.length; h++) {
-		var th = document.createElement('th');
-		th.innerHTML = orders[h];
-		tr.appendChild(th);
-	}
-	for (c = 0; c <= orders.length - 1; c++) {
-		tr = order.insertRow(-1);
-		for (j = 0; j < orders.length; j++) {
-			var td = document.createElement('td');
-			td = tr.insertCell(-1);
-			td.innerHTML = orders[c][j];
-		}
-	}
+    var row = orderTable.insertRow(-1);
+    for (var i = 0; i < 2; i++) {
+        var thCell = document.createElement("TH");
+        thCell.innerHTML = header[i];
+        row.appendChild(thCell);
+    }
+ 
+    for (var i in order.orderAmountsMap) {
+        row = orderTable.insertRow(-1);
+        var cell = row.insertCell(-1);
+        cell.innerHTML = i;
+        var cell = row.insertCell(-1);
+        cell.innerHTML = order.orderAmountsMap[i];
+        
+//        var cell = row.insertCell(-1);
+//        cell.innerHTML = order.products[i].price;
+    }
+ 
+    var dvTable = document.getElementById("tables");
+    dvTable.appendChild(document.createElement("BR"));
+    dvTable.appendChild(orderTable);
 }
 
-function addOrderItem(id,prod, q, pri) {
+function addOrderItem(id,prod) {
     orders[id].orderItems.push({Product:prod,Quantity:q,Price:pri});
 }
 
@@ -33,4 +53,27 @@ function removeOrderById(id) {
 	if (id >= orders.length) {
 		
 	}
+}
+
+function loadOrdersList(){
+    axios.get("/orders/1")
+    .then(function (response) {
+        var ordenes = response.data;
+        createOrder(ordenes);
+            
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    
+    axios.get("/orders/3")
+    .then(function (response) {
+        var ordenes = response.data;
+        createOrder(ordenes);
+            
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    //createOrder(orders);
 }
